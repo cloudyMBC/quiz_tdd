08a5809a75bab5cf63f1991bea0811b8378b453c
carolvisoto <carolvisoto@gmail.com>
2014-11-28 12:37:31 +0100

    going back to retry version of run_quiz loop

--- run_quiz.rb
+++ run_quiz.rb
@@ -1,18 +1,16 @@
 require './quiz'
-valid_answer = false
+
 q = Quiz.new
 puts q.next_question
 
-while valid_answer == false
-	begin
-		answer = gets.chomp
-		q.store_answer(answer)
-		puts "Answer was valid."
-		valid_answer = true	
-	rescue => e
-		puts e.message
-		puts "The answer must be yes or no! Please try again:"
-  end
+answer = gets.chomp
+
+begin
+	q.store_answer(answer)
+rescue
+	puts "The answer must be yes or no! Please try again:"
+	answer = gets.chomp
+	retry
 end	
  
 
